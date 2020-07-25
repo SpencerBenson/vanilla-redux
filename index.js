019700dcc84dd41a6e3569fc17863dba69bf8711
Benson Njuguna K <bennspencer@gmail.com>
2020-07-25 13:02:20 +0300

    morethan one action

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //we use require since we are using Vanilla js. If react, we'd use import statement
 const redux = require('redux');
 
-//Create the Redux store
+// initialize createStore Function
 const createStore = redux.createStore;
 
 //defining our BUY_CAKE action
 const BUY_CAKE = 'BUY_CAKE';
+const BUY_ICECREAM = 'BUY_ICECREAM';
 
 
 //action creator function. Returs the type and info.
@@ -15,13 +16,21 @@ function buyCake() {
         info: "first redux action"
     }
 }
+function buyIceCream(){
+    return{
+        type: BUY_ICECREAM,
+        info:'buy cold Icecream'
+    }
+}
+
 
 
 
 
 //Creating the initial state.
 const initialState = {
-    numOfCakes: 10
+    numOfCakes: 10,
+    numOfIcecreams: 20
 }
 
 //reducer = incharge of how the state changes.
@@ -32,19 +41,29 @@ const reducer = (state = initialState,action) => {
             ...state, // Spread initial state then update numOfCakes
             numOfCakes: state.numOfCakes - 1
         }
+        break;
+        case BUY_ICECREAM: return{
+            ...state,
+            numOfIcecreams: state.numOfIcecreams - 2
+        }
         default: return state
     }
 }
 
 
-
+// Create the Redux store
 const store = createStore(reducer);
+
 console.log('initialState: ', store.getState());
+
 const unsubscribe = store.subscribe(()=> {
     console.log('Updated State', store.getState());
 }
     );
 store.dispatch(buyCake());
 store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
 store.dispatch(buyCake());
 unsubscribe();
